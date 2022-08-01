faf5ab191329cd86622e0db37a2dffc9a40c12b8
Bradley Donahue <donahue.bradley@gmail.com>
2022-08-01 16:11:09 -0400

    Mockup first pass

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/assets/hero-image.png')",
+        'video-placeholder': "url('/assets/stockimage.png'), linear-gradient(to bottom , #ee2e1d, #ffb1b5)",
+        playbtn: "url('/assets/playbtn.png')",
+      },
+      colors: {
+        redish: '#ee2d1c',
+        blackish: '#4c4c51',
+        blueish: '#88939e',
+      },
+    },
   },
   plugins: [],
 }
