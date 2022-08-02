b295a03ca72aaa41ab40d9b1d3e2f897c14c5be4
Bradley Donahue <donahue.bradley@gmail.com>
2022-08-01 20:25:41 -0400

    Truncate long video descriptions

--- components/videoList.jsx
+++ components/videoList.jsx
@@ -9,10 +9,12 @@ export default function VideoList({ videos }) {
             <div className="mb-10">
               <h2 className="text-3xl md:text-4xl leading-9 text-blackish mb-[22px]">{video.title}</h2>
               {/* Some of the descriptions have HTML in them for formatting. This gets them to display properly. */}
-              <p
-                className="text-lg md:text-xl lg:text-[22px] leading-7"
-                dangerouslySetInnerHTML={{ __html: video.description }}
-              />
+              <div className="overflow-hidden max-h-48 relative after:absolute after:inset-0 after:from-white after:bg-gradient-to-t">
+                <p
+                  className="text-lg md:text-xl lg:text-[22px] leading-7 text-blueish"
+                  dangerouslySetInnerHTML={{ __html: video.description }}
+                />
+              </div>
             </div>
             <div className="mb-[22px] order-first lg:order-last">
               <div className="aspect-video relative object-cover">
